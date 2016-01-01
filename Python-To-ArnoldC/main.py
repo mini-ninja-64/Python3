8ab745c75e2ff17e0c0e8fa8a3a78a0d1e9965ee
mini-ninja-64 <callumhart@btinternet.com>
2016-01-01 00:24:57 +0000

    Create main.py
    no where near done obviously but input and var assignment work

---
+++ Python-To-ArnoldC/main.py
@@ -0,0 +1,31 @@
+FileLocation=input("""Python File (try not to use =, (), ", ', in strings): """)
+
+pyFile=open(FileLocation,'r')
+
+for line in pyFile:
+    formattedLine=line.replace(" ","")
+    formattedLine=formattedLine.replace('\n', '').replace('\r', '')
+    arnCEquiv=""
+    if (("=input") in formattedLine): #input
+        varName=formattedLine.split('=')[0]
+        arnCEquiv=arnCEquiv + 'CHRISTMAS TREE ' + varName + '\n'
+        arnCEquiv=arnCEquiv + '\t' + 'YOU SET US UP 0' + '\n' + '\n'
+
+        inputString = ((line.split('input')[1]).replace("'",'"').split('"'))[1]  #[:-1])[1:].replace("'",'"').replace(" ","") <-- old stuff
+        if not inputString=="":
+            arnCEquiv=arnCEquiv+'TALK TO THE HAND "' + inputString +'"'+ '\n'
+        arnCEquiv=arnCEquiv+'GET YOUR ASS TO MARS '+varName+ '\n'
+        arnCEquiv=arnCEquiv+'DO IT NOW'+'\n'
+        arnCEquiv=arnCEquiv+'I WANT TO ASK YOU A BUNCH OF QUESTIONS AND I WANT TO HAVE THEM ANSWERED IMMEDIATELY'
+    elif ((("=") in formattedLine)) and (not("if") in formattedLine) and (not("==") in formattedLine): #variable decleration
+        varName=formattedLine.split('=')[0]
+        arnCEquiv=arnCEquiv + 'CHRISTMAS TREE ' + varName + '\n'
+        arnCEquiv=arnCEquiv + '\t' + 'YOU SET US UP ' + (formattedLine.split('=')[1]) + '\n' + '\n'
+
+
+    if not line.strip():
+        print("blank line")
+    else:
+        if not (arnCEquiv == ""):
+            print("")
+            print(arnCEquiv)
